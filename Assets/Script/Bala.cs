d0c722168a59ebf0378cc50f4bae6884023ed5ce
JulianPasiArias <julianpasi97@gmail.com>
2022-06-19 14:19:18 -0300

    V 0.0.3
    Desafio 3 y 4 incluyendo el disparo y la creacion de proyectiles a traves del metodo "Instantiate"

---
+++ Assets/Script/Bala.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    
+    public GameObject prefab;
+    public Transform spawn;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+
+        {
+           Disparo();
+        }
+
+        if(Input.GetKeyDown(KeyCode.L))
+
+        {
+           Disparo();
+        }
+        if(Input.GetKeyDown(KeyCode.J))
+
+        {
+           Disparo();
+        }
+    }
+
+    void Disparo()
+    {
+        Instantiate(prefab,spawn.position,transform.rotation);
+        Debug.Log("El jugador esta disparando");
+    }
+    
+    
+
+    
+}
+
---
+++ Assets/Script/Disparo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+   public float speed = 10f;
+   public float damage;
+   public Vector3 direction;
+
+    void Update()
+    {
+        Desplazamiento();
+    }
+
+    void Desplazamiento()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
