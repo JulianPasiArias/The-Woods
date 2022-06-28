a660801520fee42484144ee0db7e14e7ec32b341
JulianPasiArias <julianpasi97@gmail.com>
2022-06-28 10:18:51 -0300

    V 0.0.5
    Desafios Incluyendo:
    -Movimiento Pj
    -Instanciamiento de Bala y cambio de Escala
    -Texturas/Modelos/Musica
    -Cambio de Camaras

---
+++ Assets/Script/Movimiento.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public GameObject camaraUno;
+    public GameObject camaraDos;
+
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+       MovimientoPj(); 
+       if(Input.GetKeyDown(KeyCode.C))
+       {
+            CambioCamara();
+       }
+       
+    }
+
+
+    void MovimientoPj()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput,0,verticalInput)* speed * Time.deltaTime);
+    }
+
+
+    void CambioCamara()
+    {
+        if (camaraUno.activeInHierarchy)
+        {
+            camaraDos.SetActive(true);
+            camaraUno.SetActive(false);
+        }
+        else
+        {
+            camaraDos.SetActive(false);
+            camaraUno.SetActive(true);
+        }
+     
+    }
+}
+
+
