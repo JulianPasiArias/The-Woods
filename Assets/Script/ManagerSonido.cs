a9d4f6785fab3650844e32c470ae5a56144a8e6c
JulianPasiArias <107800875+JulianPasiArias@users.noreply.github.com>
2022-07-28 00:43:37 +0100

    V 0.0.10
    Desafio con Singleton:
    - Public Static
    - Unica Instancia
    - Permanece en el cambio de Escena

---
+++ Assets/Script/ManagerSonido.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class ManagerSonido : MonoBehaviour
+{
+   public static ManagerSonido unicaInstancia;
+
+
+
+    void Awake()
+    {
+        if(ManagerSonido.unicaInstancia == null)
+        {
+            ManagerSonido.unicaInstancia = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    
+        
+    }
+
+    
+}
---
+++ Assets/Script/NextLevel.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+     public bool isInRange = false;
+     public AudioSource abrir;
+
+   
+    
+    void Update()
+    {
+        if(isInRange)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+          {
+            AbrirPuerta();
+            Invoke("LoadNextLevel", 1.0f);
+            
+          }
+        }
+        
+    }
+
+   void OnTriggerEnter2D(Collider2D col)
+   {
+       if(col.gameObject.CompareTag("Player"))
+       {
+           isInRange = true;
+       }
+       else
+       {
+           isInRange = false;
+       }
+   }
+
+   void AbrirPuerta()
+   {
+       abrir.Play();
+   }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
